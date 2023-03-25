ec3de49a03634deff13aa3945c600e5eb93857b7
Andrii Lisostep <reyandrey@mail.com>
2023-03-25 22:34:08 +0200

    make remark

--- src/JS/header.js
+++ src/JS/header.js
@@ -1,32 +1,17 @@
-const searchBtn = document.querySelector(".open-search-input")
-const btnContainer = document.querySelector(".btn-container")
-console.log(btnContainer)
-searchBtn.addEventListener('click', onSearchBtn)
-
-function onSearchBtn(evt) {
-    evt.preventDefault()
-    searchBtn.style.display = "none"
-    inputMarkup()
-}
-    
-function inputMarkup() {
-    const markup = '<input class="input" type="text" name="searchQury" autocomplete="off" placeholder="Search |">'
-    // console.log(typeof markup)
-    btnContainer.insertAdjacentHTML('afterbegin', markup)
-
-}
+const searchBtn = document.querySelector(".open-search-input");
+const btnContainer = document.querySelector(".btn-container");
 
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle('visually-hidden');
+    refs.menu.classList.toggle("visually-hidden");
   }
 })();
