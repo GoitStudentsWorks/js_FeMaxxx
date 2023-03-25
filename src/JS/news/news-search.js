3262e9e33c1d22017f2df1812cec1df31c206584
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-03-25 13:18:21 +0200

    main logic

---
+++ src/JS/news/news-search.js
@@ -0,0 +1,23 @@
+import SearchNewsApiService from "./news-api-search";
+
+const searchNewsApiService = new SearchNewsApiService();
+searchNewsApiService.query = "country";
+searchNewsApiService.getNews().then(searchOptimizeArray);
+let newArray = [];
+
+function searchOptimizeArray(result) {
+  result.map(obj => {
+    const array = {
+      headline: obj.headline.main,
+      abstract: obj.abstract,
+      category: obj.section_name,
+      pub_date: obj.pub_date,
+      photo: obj.multimedia,
+      url: obj.web_url,
+    };
+    newArray.push(array);
+  });
+
+  console.log(newArray);
+  return newArray;
+}
--- src/JS/news/news.js
+++ src/JS/news/news.js
@@ -1,23 +1,8 @@
-import SearchNewsApiService from "./news-api";
+//SEARCH
+import "./news-search";
 
-const searchNewsApiService = new SearchNewsApiService();
-searchNewsApiService.query = "country";
-searchNewsApiService.getNews().then(optimizeArray);
+//POPULAR
+import "./news-popular";
 
-async function optimizeArray(result) {
-  let newArray = [];
-  result.map(obj => {
-    const array = {
-      headline: obj.headline.main,
-      abstract: obj.abstract,
-      category: obj.section_name,
-      pub_date: obj.pub_date,
-      photo: obj.multimedia,
-      url: obj.web_url,
-    };
-    newArray.push(array);
-  });
-
-  console.log(newArray);
-  return newArray;
-}
+//CATEGORIES
+import "./news-categories";
